28ca7fef2fe208d98c228996e2f10484c4bab4cf
Sebastian Ullrich <sebasti@nullri.ch>
2014-08-14 00:46:10 +0200

    Implement graphics resources patching

--- Patcher/Program.cs
+++ Patcher/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using System.Linq;
+using System.IO;
+using System.Drawing;
+using System.Xml.Linq;
 
 namespace Patcher
 {
@@ -16,6 +19,74 @@ namespace Patcher
 					yield return moarNested;
 		}
 
+		static void MakeBaseImage()
+		{
+			// unseal, publicize, virtualize
+			var module = ModuleDefinition.ReadModule("OrigTowerFall.exe");
+			foreach (var type in module.GetTypes()) {
+				type.IsSealed = false;
+				foreach (var field in type.Fields)
+					field.IsPublic = true;
+				foreach (var method in type.Methods) {
+					method.IsPublic = true;
+					if (!method.IsConstructor && !method.IsStatic)
+						method.IsVirtual = true;
+				}
+			}
+			module.Write("BaseTowerFall.exe");
+		}
+
+		static void PatchBaseImage()
+		{
+			var baseModule = ModuleDefinition.ReadModule("BaseTowerFall.exe");
+			var modModule = ModuleDefinition.ReadModule("Mod.dll");
+			// replace all object creations in BaseTowerFall with the respective derived class in Mod, if any.
+			var typeMapping = modModule.Types.Where(t => t.BaseType != null).ToDictionary(t => t.BaseType.FullName);
+			foreach (TypeDefinition type in baseModule.Types.SelectMany(AllNestedTypes))
+				foreach (var method in type.Methods)
+					if (method.HasBody)
+						foreach (var instr in method.Body.Instructions)
+							if (instr.OpCode == OpCodes.Newobj) {
+								var ctor = (MethodReference)instr.Operand;
+								if (typeMapping.ContainsKey(ctor.DeclaringType.FullName))
+									instr.Operand = baseModule.Import(typeMapping[ctor.DeclaringType.FullName].Methods.Single((m => m.IsConstructor)));
+							}
+			baseModule.Write("TowerFall.exe");
+		}
+
+		static void PatchResources()
+		{
+			foreach (var atlasPath in Directory.EnumerateDirectories(Path.Combine("Content", "Atlas"))) {
+				File.Copy(Path.Combine("Original", atlasPath + ".png"), atlasPath + ".png", overwrite: true);
+				File.Copy(Path.Combine("Original", atlasPath + ".xml"), atlasPath + ".xml", overwrite: true);
+
+				var xml = XElement.Load(atlasPath + ".xml");
+
+				string[] files = Directory.GetFiles(atlasPath, "*.png", SearchOption.AllDirectories);
+				int x = 700;
+
+				using (var baseImage = Bitmap.FromFile(atlasPath + ".png")) {
+					using (var g = Graphics.FromImage(baseImage))
+						foreach (string file in files)
+							using (var image = Bitmap.FromFile(file)) {
+								string name = file.Substring(atlasPath.Length + 1).Replace(Path.DirectorySeparatorChar, '/');
+								name = name.Substring(0, name.Length - ".png".Length);
+								g.DrawImage(image, x, 1700);
+								xml.Add(new XElement("SubTexture",
+									new XAttribute("name", name),
+									new XAttribute("x", x),
+									new XAttribute("y", 1700),
+									new XAttribute("width", image.Width),
+									new XAttribute("height", image.Height)
+								));
+								x += image.Width;
+							}
+					baseImage.Save(atlasPath + ".png");
+				}
+				xml.Save(atlasPath + ".xml");
+			}
+		}
+
 		public static void Main (string[] args)
 		{
 			if (args.Length != 1) {
@@ -23,35 +94,10 @@ namespace Patcher
 				return;
 			}
 			if (args[0] == "makeBaseImage") {
-				// unseal, publicize, virtualize
-				var module = ModuleDefinition.ReadModule("OrigTowerFall.exe");
-				foreach (var type in module.GetTypes()) {
-					type.IsSealed = false;
-					foreach (var field in type.Fields)
-						field.IsPublic = true;
-					foreach (var method in type.Methods) {
-						method.IsPublic = true;
-						if (!method.IsConstructor && !method.IsStatic)
-							method.IsVirtual = true;
-					}
-				}
-				module.Write("BaseTowerFall.exe");
+				MakeBaseImage();
 			} else {
-				var baseModule = ModuleDefinition.ReadModule("BaseTowerFall.exe");
-				var modModule = ModuleDefinition.ReadModule("Mod.dll");
-
-				// replace all object creations in BaseTowerFall with the respective derived class in Mod, if any.
-				var typeMapping = modModule.Types.Where(t => t.BaseType != null).ToDictionary(t => t.BaseType.FullName);
-				foreach (TypeDefinition type in baseModule.Types.SelectMany(AllNestedTypes))
-					foreach (var method in type.Methods)
-						if (method.HasBody)
-							foreach (var instr in method.Body.Instructions)
-								if (instr.OpCode == OpCodes.Newobj) {
-									var ctor = (MethodReference)instr.Operand;
-									if (typeMapping.ContainsKey(ctor.DeclaringType.FullName))
-										instr.Operand = baseModule.Import(typeMapping[ctor.DeclaringType.FullName].Methods.Single((m => m.IsConstructor)));
-								}
-				baseModule.Write("TowerFall.exe");
+				PatchBaseImage();
+				PatchResources();
 			}
 		}
 	}
--- README.md
+++ README.md
@@ -6,11 +6,11 @@ A mod framework for [TowerFall Ascension](http://www.towerfall-game.com/) (copyr
 Hacking
 =======
 
-* Copy your TowerFall.exe to bin/OrigTowerFall.exe
+* Copy Steam/SteamApps/common/TowerFall to bin/Original (or at least TowerFall.exe and Content/Atlas, to save some copying time)
 * Compile Patcher and run `./Patcher.exe makeBaseImage` in bin/  
   This will create a BaseTowerFall.exe where all classes are unsealed, all fields public and all methods virtual public, so you can meaningfully derive from any TowerFall class.
 * Now you can compile Mod, which depends on BaseTowerFall.exe
 * Run `./Patcher.exe patchBaseImage` to replace all object creations in BaseTowerFall.exe with the respective class in Mod.dll, if any
-* Copy (or just symlink) the resulting TowerFall.exe and Mod.dll back to the TowerFall Steam directory.
+* Copy (or just symlink) the resulting TowerFall.exe, Content/ and Mod.dll back to the TowerFall Steam directory.
 
-Whenever you have added or removed derived classes to/from Mod, re-run patchBaseImage.
+Whenever you have added or removed derived classes or resources to/from Mod, re-run patchBaseImage.
